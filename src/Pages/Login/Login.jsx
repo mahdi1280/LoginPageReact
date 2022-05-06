10356681c6ba689e243a5f2305032d26434dd263
mahdikarimi422@gmail.com <09388990352@>
2022-05-06 16:37:44 +0430

    login complite.

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,29 +1,71 @@
-import React,{useState} from 'react';
+import React, {useEffect, useState,useLayoutEffect} from 'react';
 import './style.css';
 import axios from "axios";
+import {useAuthDispatch} from '../../Context/auth-context';
+import {actionTypes} from '../../Context/reduce';
 
-const fetchToken= async (username,password)=>{
-    return axios.post("http://localhost:3001/login",{
-        username:username,
-        password:password
-    })
+const fetchToken = async (username, password) => {
+    return axios.post("http://localhost:3001/login", {
+        username: username,
+        password: password
+    }).then(response => response.data)
 }
 
-export default function Login(){
-    const [username,setUsername] = useState('');
-    const [password,setPassword] = useState('');
+const fetchCurrentUserInfo = async (token) => {
+    return axios.get("http://localhost:3001/users/me", {
+        headers: {
+            Authorization: token
+        }
+    }).then(response => response.data);
+}
 
-    const handleLogin = (e)=>{
+export default function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [token, setToken] = useState(null);
+    const dispatch = useAuthDispatch();
+
+    const handleLogin = (e) => {
         e.preventDefault();
-        fetchToken(username,password)
-            .then(console.log);
+        fetchToken(username, password)
+            .then(({success, data}) => {
+                if (success) {
+                    setToken(data);
+                }
+            });
     }
 
-    return ( <div className="login">
+    useLayoutEffect(()=>{
+        const token = localStorage.getItem("token");
+        if(token){
+            setToken(token);
+        }
+    },[])
+    useLayoutEffect(() => {
+        if (token) {
+            fetchCurrentUserInfo(token)
+                .then(({data, success}) => {
+                    if (success) {
+                        localStorage.setItem('token',token);
+                        dispatch({
+                                type: actionTypes.LOGIN_SUCCESS,
+                                payload: {
+                                    user: data,
+                                    token: token
+                                }
+                            });
+                    }
+                });
+        }
+    });
+
+    return (<div className="login">
         <h1>Login</h1>
         <form onSubmit={handleLogin} method="post">
-            <input onChange={(e)=>setUsername(e.target.value)} value={username} type="text" placeholder="Username" required="required" />
-            <input onChange={(e)=>setPassword(e.target.value)} value={password} type="password" placeholder="Password" required="required" />
+            <input onChange={(e) => setUsername(e.target.value)} value={username} type="text" placeholder="Username"
+                   required="required"/>
+            <input onChange={(e) => setPassword(e.target.value)} value={password} type="password" placeholder="Password"
+                   required="required"/>
             <button type="submit" className="btn btn-primary btn-block btn-large">Let me in.</button>
         </form>
     </div>);
